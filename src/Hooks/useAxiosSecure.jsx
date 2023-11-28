789f4598e9a6d3ad326f281b6dc51b5058d3ae7c
arafatah <arafathowlader456@gmail.com>
2023-11-28 16:33:54 +0600

    jwt added to the project

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,41 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 export const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      console.log("object");
+      const token = localStorage.getItem("access-token");
+      if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (err) => Promise.reject(err)
+  );
+
+  // intersect 401 error
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error?.response?.status;
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
--- src/Pages/Dashboard/AdminHome/ManageUser.jsx
+++ src/Pages/Dashboard/AdminHome/ManageUser.jsx
@@ -3,97 +3,97 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import Swal from "sweetalert2";
 
 const ManageUser = () => {
-    const axiosSecure = useAxiosSecure();
-    const { refetch, data: users = [] } = useQuery({
-        queryKey: ["users"],
-        queryFn: async () => {
-            const res = await axiosSecure.get("/users");
-            return res.data;
-        }
+  const axiosSecure = useAxiosSecure();
+  const { refetch, data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+
+  // Function to handle making a user an admin
+  const handleMakeAdmin = (user) => {
+    // Logic for making the user an admin
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      if (res.data.modifiedCount === 1) {
+        refetch();
+        Swal.fire({
+          title: "Success",
+          text: "User has been made an admin",
+          icon: "success",
+          confirmButtonText: "Ok",
+        });
+      }
+      console.log(res);
     });
 
-    // Function to handle making a user an admin
-    const handleMakeAdmin = (user) => {
-        // Logic for making the user an admin
-        axiosSecure.patch(`/users/admin/${user._id}`)
-        .then(res => {
-            if(res.data.modifiedCount === 1){
-                refetch();
-                Swal.fire({
-                    title: "Success",
-                    text: "User has been made an admin",
-                    icon: "success",
-                    confirmButtonText: "Ok",
-                })
+    console.log(`Making user with ID ${user._id} an admin`);
+  };
 
-            }
-            console.log(res)
-        })
+  // Function to handle making a user a tour guide
+  const handleMakeTourGuide = (user) => {
+    // Logic for making the user a tour guide
+    axiosSecure.patch(`/users/guide/${user._id}`).then((res) => {
+      if (res.data.modifiedCount === 1) {
+        refetch();
+        Swal.fire({
+          title: "Success",
+          text: "User has been made an admin",
+          icon: "success",
+          confirmButtonText: "Ok",
+        });
+      }
+      console.log(res);
+    });
+    console.log(`Making user with ID ${user} a tour guide`);
+  };
 
-
-        console.log(`Making user with ID ${user._id} an admin`);
-    };
-
-    // Function to handle making a user a tour guide
-    const handleMakeTourGuide = (user) => {
-        // Logic for making the user a tour guide
-        axiosSecure.patch(`/users/guide/${user._id}`)
-        .then(res => {
-            if(res.data.modifiedCount === 1){
-                refetch();
-                Swal.fire({
-                    title: "Success",
-                    text: "User has been made an admin",
-                    icon: "success",
-                    confirmButtonText: "Ok",
-                })
-
-            }
-            console.log(res)
-        })
-        console.log(`Making user with ID ${user} a tour guide`);
-    };
-
-    return (
-        <div>
-            <h2 className="text-2xl font-bold mb-4">Manage Users</h2>
-            <table className="min-w-full border border-gray-300">
-                <thead>
-                    <tr>
-                        <th className="border bg-gray-100 p-2">User Name</th>
-                        <th className="border bg-gray-100 p-2">User Email</th>
-                        <th className="border bg-gray-100 p-2">Role</th>
-                        <th className="border bg-gray-100 p-2">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user) => (
-                        <tr key={user._id}>
-                            <td className="border p-2">{user.name}</td>
-                            <td className="border p-2">{user.email}</td>
-                            <td className="border p-2">{user.role}</td>
-                            <td className="border p-2">
-                                <button
-                                    onClick={() => handleMakeAdmin(user)}
-                                    disabled={user.role === 'Admin'}
-                                    className={`bg-blue-500 text-white px-4 py-2 rounded mr-2 ${user.role === 'Admin' && 'opacity-50 cursor-not-allowed'}`}
-                                >
-                                    Make Admin
-                                </button>
-                                <button
-                                    onClick={() => handleMakeTourGuide(user)}
-                                    disabled={user.role === 'Tour Guide'}
-                                    className={`bg-green-500 text-white px-4 py-2 rounded ${user.role === 'Tour Guide' && 'opacity-50 cursor-not-allowed'}`}
-                                >
-                                    Make Tour Guide
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Manage Users</h2>
+      <table className="min-w-full border border-gray-300">
+        <thead>
+          <tr>
+            <th className="border bg-gray-100 p-2">User Name</th>
+            <th className="border bg-gray-100 p-2">User Email</th>
+            <th className="border bg-gray-100 p-2">Role</th>
+            <th className="border bg-gray-100 p-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td className="border p-2">{user.name}</td>
+              <td className="border p-2">{user.email}</td>
+              <td className="border p-2">{user.role}</td>
+              <td className="border p-2">
+                <button
+                  onClick={() => handleMakeAdmin(user)}
+                  disabled={user.role === "Admin"}
+                  className={`bg-blue-500 text-white px-4 py-2 rounded mr-2 ${
+                    user.role === "Admin" && "opacity-50 cursor-not-allowed"
+                  }`}
+                >
+                  Make Admin
+                </button>
+                <button
+                  onClick={() => handleMakeTourGuide(user)}
+                  disabled={user.role === "Tour Guide"}
+                  className={`bg-green-500 text-white px-4 py-2 rounded ${
+                    user.role === "Tour Guide" &&
+                    "opacity-50 cursor-not-allowed"
+                  }`}
+                >
+                  Make Tour Guide
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ManageUser;
